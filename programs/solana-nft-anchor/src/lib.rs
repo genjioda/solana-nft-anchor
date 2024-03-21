859e5d57bcda5d903d46bbfcb51f04f455857d9a
genjioda <164327720+genjioda@users.noreply.github.com>
2024-03-21 16:24:22 +0700

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("9gveKFLsrswTgF34PCNMC5vsuc2VepiMbnrDnejt9VNm");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
